8e02005059187b9d56f02c273826979f8b533f2a
matija kovacic <info@mkovacic.com>
2025-03-08 14:05:57 +0100

    dorada

--- android/app/src/main/java/io/ionic/starter/MainActivity.java
+++ android/app/src/main/java/io/ionic/starter/MainActivity.java
@@ -1,4 +1,4 @@
-package io.ionic.starter;
+package com.beaverGame.prolink;
 
 import com.getcapacitor.BridgeActivity;
 
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.beaverGame',
+  appId: 'com.beaverGame.prolink',
   appName: 'Dabar-Beaver',
   webDir: 'www'
 };
